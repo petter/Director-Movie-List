f88fc4a793ba09055f8879b20bff84b55d15949c
Petter Sæther Moen <pettersmoen@gmail.com>
2019-10-07 15:19:21 +0200

    Updates

---
+++ precache-manifest.aaf8ef66862d72897f13c6cb243f4c19.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b070542d05934e48f810b8ade4d5adc",
+    "url": "/Director-Movie-List/index.html"
+  },
+  {
+    "revision": "3a4d79d0503efafb1426",
+    "url": "/Director-Movie-List/static/css/main.c9da937e.chunk.css"
+  },
+  {
+    "revision": "4a3db175cdf771b56468",
+    "url": "/Director-Movie-List/static/js/2.d3360293.chunk.js"
+  },
+  {
+    "revision": "3a4d79d0503efafb1426",
+    "url": "/Director-Movie-List/static/js/main.da62a565.chunk.js"
+  },
+  {
+    "revision": "4cb342af48751fa79787",
+    "url": "/Director-Movie-List/static/js/runtime~main.2f0da69e.js"
+  },
+  {
+    "revision": "ac5db5766a0e69259461334ca3da946d",
+    "url": "/Director-Movie-List/static/media/check-square-solid.ac5db576.svg"
+  },
+  {
+    "revision": "9a983e891bbb901599706c10ff39177e",
+    "url": "/Director-Movie-List/static/media/question-square-solid.9a983e89.svg"
+  }
+]);
